6fb8277190b1791d91c2a064a7bac8c491cebf37
RUDRANSH CHANDEL <160801789+RudranshChandell@users.noreply.github.com>
2026-02-12 11:14:26 +0530

    initial commit

---
+++ src/main/java/com/Teleport/smartload/SmartloadApplication.java
@@ -0,0 +1,13 @@
+package com.Teleport.smartload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Teleport/smartload/SmartloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teleport.smartload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
